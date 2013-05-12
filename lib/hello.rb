14ce6ce7000460cb31f22cfd0c43737b0c1c6e44
gcruz <gcruz@bluemessaging.com>
2013-05-11 20:25:31 -0500

    Hello uses greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,9 @@
+require 'greeter'
+
 #Default is world
 # Author: Gibran Cruz (gcruz@bluemessaging.com)
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+
+puts greeter.greet
